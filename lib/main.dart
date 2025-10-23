a165b2fe35b934940fd3ffe49486025fde85e91a
Eugine Onyango <eugineogutu004@gmail.com>
2025-10-23 13:08:20 +0300

    Added the 4-homepage widgets

--- lib/main.dart
+++ lib/main.dart
@@ -74,12 +74,114 @@ class HomeScreen extends StatelessWidget {
                   ),
                     
                 ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 30.0),
+                  padding: EdgeInsets.symmetric(horizontal: 30.0,vertical: 5.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(29.5),
+                  ),
+                  
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      icon: SvgPicture.asset("assets/icons/search.svg"),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    childAspectRatio: .85,
+                    crossAxisSpacing: 20.0,
+                    mainAxisSpacing: 20.0,
+                    children: [
+                      CategoryCard(
+                        title: "Diet Recommendation",
+                        svgSrc: "assets/icons/Hamburger.svg",
+                        press: () {} ,
+                      ),
+                      CategoryCard(
+                        title: "Kegel Exercises",
+                        svgSrc: "assets/icons/Excrecises.svg", 
+                        press: () {} ,
+                      ),
+                      CategoryCard(
+                        title: "Meditation",
+                        svgSrc: "assets/icons/Meditation.svg",
+                        press: () {} ,
+                      ),
+                      CategoryCard(
+                        title: "Yoga",
+                        svgSrc: "assets/icons/yoga.svg",
+                        press: () {} ,
+                      ),
+
+                    ],
+                  ),
+                ),
                 ],
                         ),
             ),
         )
 
         ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String svgSrc;
+  final String title;
+  final VoidCallback press;
+  const CategoryCard({
+    super.key, required this.svgSrc, required this.title, required this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(13.0),
+      child: Container(
+        //padding: EdgeInsets.all(  20.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(13.0),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0,17),
+              blurRadius: 17.0,
+              spreadRadius: -23.0,
+              color: kShadowColor,
+            ),
+          ],
+        ),
+        child:Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: press,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Spacer(),
+                  SvgPicture.asset(svgSrc),
+                  Spacer(),
+                  Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                    .textTheme
+                    .titleMedium
+                    ?.copyWith(fontSize: 15.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
